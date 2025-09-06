edf8e931d5f5b079d4fc1fac7bf92236ba78023f
YassinOs-dot <yassineabdelwahed11@gmail.com>
2025-09-06 18:48:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,25 @@ image_rocauc, pixel_rocauc, au_pro = patchcore.evaluate()
 image_rocaucs_df[CLASS_NAME.title()] = [image_rocauc]
 pixel_rocaucs_df[CLASS_NAME.title()] = [pixel_rocauc]
 au_pros_df[CLASS_NAME.title()] = [au_pro]
+
+# Print results
+print("\n\n############################# Image ROCAUC Results #############################\n")
+print(image_rocaucs_df.to_markdown(index=False))
+
+print("\n\n############################# Pixel ROCAUC Results #############################\n")
+print(pixel_rocaucs_df.to_markdown(index=False))
+
+print("\n\n############################# AU PRO Results #############################\n")
+print(au_pros_df.to_markdown(index=False))
+
+
+# Save results
+os.makedirs("results", exist_ok=True)
+image_rocaucs_df.to_markdown("results/image_rocauc_results.md", index=False)
+pixel_rocaucs_df.to_markdown("results/pixel_rocauc_results.md", index=False)
+au_pros_df.to_markdown("results/aupro_results.md", index=False)
+
+
+   
+
+
